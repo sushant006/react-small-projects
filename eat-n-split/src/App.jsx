329c892971875c5218db1b1207d649cd59c7ce11
sushant006 <sushantpathak98189@gmail.com>
2024-06-01 17:13:34 +0530

    added dynamic form button

--- eat-n-split/src/App.jsx
+++ eat-n-split/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const initialFriends = [
   {
     id: 118836,
@@ -18,14 +19,23 @@ const initialFriends = [
     balance: 0,
   },
 ];
+function Button({children, onClick})
+{
+  return <button className="button" onClick={onClick}>{children}</button>
+}
 
 export default function App() {
+  const[showAddFriend,setShowAddFriend] = useState(false);
+  function handleShowAddFriend()
+  {
+    setShowAddFriend((show) => !show)
+  }
   return(
   <div className="app">
     <div className="sidebar">
       <FriendList/>
-      <FormAddFriend/>
-      <Button>Add Friend</Button>
+      {showAddFriend && <FormAddFriend/>}
+      <Button onClick={handleShowAddFriend}>{showAddFriend ? "Close" : "Add Friend"}</Button>
     </div>
     <FormSplitBill/>
   </div>
@@ -64,10 +74,7 @@ function Friend({friend})
     </li>
   );
 }
-function Button({children})
-{
-  return <button className="button">{children}</button>
-}
+
 function FormAddFriend() {
   return (
     <form className="form-add-friend">
